286c048c51ba769be3de3f6c7875bda47cf4ba66
kaikaige <396929174@qq.com>
2017-06-05 17:52:59 +0800

    测试镜像速度

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 <strong>GitHub: </strong><a href="https://github.com/skiy-dockerfile/nginx-php7" target="_blank">https://github.com/skiy-dockerfile/nginx-php7</a><br />
 <strong>Docker Hub: </strong><a href="https://hub.docker.com/r/skiychan/nginx-php7" target="_blank">https://hub.docker.com/r/skiychan/nginx-php7</a><br />  
 </div>
-
+<h1>kaikaige</h1>
 <?php
 phpinfo();
